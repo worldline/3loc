8e0b90f617cae3f348a85fb1a2d558b0f23d6bd2
Nicolas Morel <nicolas@morel.io>
2016-03-07 17:13:19 +0000

    Fix linting

--- src/helpers/unquote.js
+++ src/helpers/unquote.js
@@ -2,9 +2,12 @@
 
 /**
  * Nunjucks helper to remove quotes from strings
+ *
+ * @param {String} value - original value
+ * @return {String} new string with meta information about unquoting
  */
 module.exports = function unquote(value) {
-    const str = new String(value);
-    str.unquote = true;
-    return str;
-}
+  const str = new String(value); // eslint-disable-line no-new-wrappers
+  str.unquote = true;
+  return str;
+};
